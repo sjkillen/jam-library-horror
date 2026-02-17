a092aea372a20d3c0567b1448abb49a8c0fbdd63
cat-bevan <70996599+cat-bevan@users.noreply.github.com>
2026-02-17 16:37:52 -0700

    Initial Wwise integration

---
+++ Wwise/resources/wwise_ids.gd
@@ -0,0 +1,42 @@
+class_name AK
+
+class EVENTS:
+
+	const PLAY_MX_GAMEPLAY_DAY_LOOP : int = 1694637492
+
+class STATES:
+	pass
+
+class SWITCHES:
+	pass
+
+class GAME_PARAMETERS:
+	pass
+
+class TRIGGERS:
+	pass
+
+class BANKS:
+
+	const SB_PLACEHOLDERS : int = 4245214463
+
+class AUX_BUSSES:
+	pass
+
+class ACOUSTIC_TEXTURES:
+
+	const ACOUSTIC_BANNER : int = 4168643977
+	const ANECHOIC : int = 1873957695
+	const BRICK : int = 504532776
+	const CARPET : int = 2412606308
+	const CONCRETE : int = 841620460
+	const CORK_TILES : int = 3195498748
+	const CURTAINS : int = 2928161104
+	const DRYWALL : int = 3670307564
+	const FABRIC : int = 1970351858
+	const MOUNTAIN : int = 513139656
+	const TILE : int = 2637588553
+	const WOOD : int = 2058049674
+	const WOOD_BRIGHT : int = 4262522749
+	const WOOD_DEEP : int = 1755085759
+
